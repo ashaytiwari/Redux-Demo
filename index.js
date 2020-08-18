1b4e863ef16c2e445d09cf8e8aecaddd6f783eb0
ashaytiwari <ashaytiwari2001@gmail.com>
2020-08-18 18:44:46 +0530

    async action managed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -57,9 +61,9 @@ const rootReducer = combineReducers({
     icecream: icecreamReducer
 })
 //Responsibilties of store
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('Initial state', store.getState())
-const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()))
+const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
